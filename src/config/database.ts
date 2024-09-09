55a3a2ab5828266766ee59420114edddaceeed58
hugoty <hugo.raoult@hotmail.fr>
2024-09-09 17:52:10 +0200

    change db

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,15 @@
-// src/config/database.ts
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("koliseum", "root", "root", {
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "koliseum",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "root",
+  {
     host: process.env.DB_HOST || "localhost",
-    dialect: "mariadb",
-    port: Number(process.env.DB_PORT) || 3306,
+    dialect: "postgres",
+    port: Number(process.env.DB_PORT) || 5432,
     logging: console.log,
-});
+  }
+);
 
 export default sequelize;
